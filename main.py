e563c9b9743ca3a5753fb588703a4901e9b0c9b6
YuLian <yulian9688@gmail.com>
2021-04-11 17:07:47 +0900

    updata

--- main.py
+++ main.py
@@ -1,45 +1,24 @@
-import random
+from Level import *
+level = Level(USE_BATCHING=50)
 
-import mapUtils
-import interfaceUtils
-from worldLoader import WorldSlice
-from nbt import *
+area = level.getBuildArea()
+x_start = area[0]
+z_start = area[1]
+x_size = area[2]
+z_size = area[3]
+x_end = x_start + x_size
+z_end = z_start + z_size
+x_center = int((x_start + x_end) /2)
+z_center = int((z_start + z_end) /2)
 
-area = (0, 0, 128, 128)  # default build area
-USE_BATCHING = True
-buildArea = interfaceUtils.requestBuildArea()
-if buildArea != -1:
-    x1 = buildArea["xFrom"]
-    z1 = buildArea["zFrom"]
-    x2 = buildArea["xTo"]
-    z2 = buildArea["zTo"]
-    # print(buildArea)
-    area = (x1, z1, x2-x1, z2-z1)
+from buildingData import writeData, buildingData
 
-print("Build area is at position %s, %s with size %s, %s" % area)
-
-worldSlice = WorldSlice(area)
-
-heightmap = mapUtils.calcGoodHeightmap(worldSlice)
+b = buildingData(level,filename="house.txt")
+b.build(x_center,level.getHeightAt(x_center,z_center),z_center)
 
 
-def heightAt(x, z):
-    return heightmap[(x - area[0], z - area[1])]
+import time
 
-
-def setBlock(x, y, z, block):
-    if USE_BATCHING:
-        interfaceUtils.placeBlockBatched(x, y, z, block, 100)
-    else:
-        interfaceUtils.setBlock(x, y, z, block)
-
-x_start =area[0]
-z_start =area[1]
-x_size =area[2]
-z_size =area[3]
-
-from buildingData import *
-writeData(worldSlice,[130,4,13],[154,45,37],"tower")
-# b = buildingData("tower.txt")
-# #b.build(0,4,0)
-# b.clear(0,4,0)
+time.sleep(30)
+print("redo")
+level.redo()
